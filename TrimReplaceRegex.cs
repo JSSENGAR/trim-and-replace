cfb6f680a3c0e5bbef672ab813f68a8ed450bad3
Jitendra Singh Sengar <40722850+JSSENGAR@users.noreply.github.com>
2019-07-06 14:57:27 +0530

    Update TrimReplaceRegex.cs

--- TrimReplaceRegex.cs
+++ TrimReplaceRegex.cs
@@ -1,19 +1,15 @@
 using System;
 using System.Text.RegularExpressions;
 
-class program 
-{    
-//TrimReplaceRegex
-    static void Main()
-    {        
-		// for .cs --  
-		
-		// Response.Write(Regex.Replace("....''' jitendra singh sengar  9589391358 ****'".Trim('*', '.', '\'', ' ').Replace("\'", ""), @"\d", ""));  
-		
-		Console.WriteLine(Regex.Replace("....''' jitendra singh sengar  9589391358 ****'".Trim('*', '.', '\'', ' ').Replace("\'", ""), @"\d", ""));
-		
-    }
+
+namespace TrimReplaceRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Regex.Replace("....''' jitendra singh sengar  9589391358 ****'".Trim('*', '.', '\'', ' ').Replace("\'", ""), @"\d", ""));
+        }
+    }    
 }
 
-//Output
-//jitendra singh sengar
